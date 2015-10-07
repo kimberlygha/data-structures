592d0d9d3de1b358e2bfb375e10517fd9b488ec3
kimberlygha <kimberlygha@gmail.com>
2015-10-07 15:32:52 -0700

    Completed functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,16 +2,15 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {
-
+    storage : {},
+    length : 0
   };
+  // Fawn's note: It's ok for storage to be public in all except functional
 
-  someInstance.prototype = stackMethods;
+  // extend someInstance with keys in stackMethods
+  extend(someInstance, stackMethods);
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
-  var length = 0;
-
   return someInstance;
 };
 
@@ -19,24 +18,30 @@ var Stack = function() {
 var stackMethods = {
 
   push : function(value) {
-    storage[length] = value;
-    length++;
+    this.storage[this.length] = value;
+    this.length++;
   },
 
   pop : function() {
     var result;
-    if (length > 0) {
-    result = storage[length-1];
-    delete storage[length-1];
-    length--;
+    if (this.length > 0) {
+    result = this.storage[this.length-1];
+    delete this.storage[this.length-1];
+    this.length--;
     }
     return result;
   },
 
   size : function() {
-    return length;
+    return this.length;
   }
 
 };
 
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
+// Fawn's note: "storage" as a private variable in Stack would not have been accessible from stackMethods without "this", it seems
