5d3ca9a1f5dae79e76e5a264bace597949b548c7
kimberlygha <kimberlygha@gmail.com>
2015-10-09 22:44:03 -0700

    Finished hash table (with excepting of pending specs).

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,14 +6,36 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  // debugger;
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // if this._storage[index] is undefined 
+  //var bucket = this._storage[index]; //bucket broke everything for some reason, so ignore this line. why?
+  // if this._storage[index] (bucket) is undefined 
   if(this._storage[index] === undefined){
     // set this._storage[index] to an empty array 
     this._storage[index] = []; 
+    // store [k,v] in this._storage[index]
+    this._storage[index].push([k,v]);
+  } else {
+  //else (if bucket already exists)
+    // declare variable alreadyExists and set to false
+    var alreadyExists = false;
+    // iterate over each element in this._storage[index]
+    for (var i = 0; i < this._storage[index].length; i++) {
+      // if current element[0] equals k
+      var currentEl = this._storage[index][i]
+      if (currentEl[0] === k) {
+        // set current element[1] to v (overwrite values that have same key)
+        currentEl[1] = v;
+        // change alreadyExists variable to true
+        alreadyExists = true;
+      }
+    }
+    // if alreadyExists is false
+    if (alreadyExists === false) {
+      // store [k,v] in this._storage[index]
+      this._storage[index].push([k,v]);
+    } 
   }
-  // store [k,v] in this._storage[index]
-  this._storage[index].push([k,v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
